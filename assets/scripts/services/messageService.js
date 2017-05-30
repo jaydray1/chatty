d5b325eb8d7167601b49bf8fe36cf03dafc61943
JM <jdreher15@gmail.com>
2017-05-30 14:22:00 -0600

    fresh

--- assets/scripts/services/messageService.js
+++ assets/scripts/services/messageService.js
@@ -1,14 +1,15 @@
-'use strict';
+
 
 angular.module('chattyApp')
   .service('messageService', function ( $http ) {
-    
+
       this.getMessages = function () {
-        return $http.get('/messages');
+        return $http.get('/messages').then(function(response){
+        })
       }
 
       this.addMessage = function ( message ) {
         return $http.post('/messages', { message: message });
       }
-    
+
   });
